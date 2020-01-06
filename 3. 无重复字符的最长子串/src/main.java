409aefb44d18dfbff30197dd7fc913f45b83d85d
Jacket <117010420@link.cuhk.edu.cn>
2020-01-07 00:54:58 +0800

    Finish Q66

---
+++ 3. 无重复字符的最长子串/src/main.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class main {
+}
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int inside = 0; // length of no-duplicate substring inside the string
+        int inside_start = 0;
+        int inside_end = 0;
+
+        int last = 0; // length of no-duplicate substring on the last of the string
+        int last_start = 0;
+        int last_end = 0;
+
+        Map inside_map = new HashMap();
+        Map last_map = new HashMap();
+
+        for (int idx=0; idx<s.length(); idx++){
+
+            }
+        }
+    }
+}
---
+++ 66. 加一/src/main.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+
+public class main {
+    public static void main(String[] args) {
+        Solution solu = new Solution();
+        int[] arr = {4, 3, 2, 1};
+        arr = solu.plusOne(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        return add_carry(digits, len - 1);
+    }
+
+    public static int[] add_carry(int[] digits, int pos) {
+        if (pos == 0) {
+            if (digits[0] == 9) {
+                digits[0] = 0;
+                int[] new_digits = new int[digits.length + 1];
+                new_digits[0] = 1;
+                for (int i = 0; i < digits.length; i++) new_digits[i + 1] = digits[i];
+                return new_digits;
+            }
+            else{
+                digits[0] = digits[0]+1;
+                return digits;
+            }
+        }
+
+        if (digits[pos] == 9) {
+            digits[pos] = 0;
+            pos--;
+            digits = add_carry(digits, pos);
+        } else digits[pos] = digits[pos] + 1;
+
+        return digits;
+    }
+}
