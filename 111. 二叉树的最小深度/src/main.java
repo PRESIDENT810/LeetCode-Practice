346678d6690c94a8896232d3856440f2b23b1de3
Jacket <117010420@link.cuhk.edu.cn>
2020-01-16 02:14:53 +0800

    Finish Q111

---
+++ 111. 二叉树的最小深度/src/main.java
@@ -0,0 +1,36 @@
+public class main {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        int min = Integer.MAX_VALUE;
+
+        return minDepth(root, min);
+    }
+
+    public int minDepth(TreeNode root, int min) {
+        if (root.left == null && root.right == null) {
+            return 1;
+        }
+
+        int left_depth, right_depth;
+        if (root.left != null) left_depth = minDepth(root.left, min);
+        else left_depth = Integer.MAX_VALUE;
+        if (root.right != null) right_depth = minDepth(root.right, min);
+        else right_depth = Integer.MAX_VALUE;
+
+        int crt_depth = Math.min(left_depth, right_depth) + 1;
+        return Math.min(min, crt_depth);
+    }
+}
