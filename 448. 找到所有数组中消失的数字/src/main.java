98bf5f1d4771e097d44a6e74996d2c03c19ffc0c
Jacket <117010420@link.cuhk.edu.cn>
2020-01-16 22:01:28 +0800

    Finish Q448 (O(1) space complexity)

--- 448. 找到所有数组中消失的数字/src/main.java
+++ 448. 找到所有数组中消失的数字/src/main.java
@@ -2,63 +2,36 @@ import java.util.*;
 
 public class main {
     public static void main(String args[]){
-        int[] nums = {1,1};
-        System.out.print(new Solution().findDisappearedNumbers(nums).toString());
+        int[] nums = {4,3,2,7,8,2,3,1};
+        System.out.print(new Solution448().findDisappearedNumbers(nums).toString());
     }
 }
 
+class Solution448 {
+    public List<Integer> add(List<Integer> ans, int last, int current) {
+        for (int i = 1; i < current - last; i++) ans.add(i + last);
 
-class Solution {
-    public Set add_plus(Set set, int max, int current){
-        for (int i=1; i<current-max; i++){
-            set.add(i+max);
-        }
-        return set;
-    }
-
-    public Set add_minus(Set set, int min, int current){
-        for (int i=1; i<min-current; i++){
-            set.add(i+current);
-        }
-        return set;
-    }
-
-    public List<Integer> convert(Set set){
-        List<Integer> ans = new LinkedList<>();
-        set.forEach(e -> ans.add((Integer) e));
-
-//        Iterator iter = set.iterator();
-//        while(iter.hasNext()){
-//            ans.add((Integer) iter.next());
-//        }
         return ans;
     }
 
     public List<Integer> findDisappearedNumbers(int[] nums) {
         if (nums.length == 0) return new LinkedList<>();
 
-        int max = nums[0];
-        int min = nums[0];
-        int len = 0;
-        Set set = new HashSet();
+        Arrays.sort(nums);
+        List<Integer> ans = new LinkedList<>();
 
-        for (int idx=0; idx<nums.length; idx++){
-            int current_num = nums[idx];
+        int idx = 0;
+        int last = 0;
 
-            if (set.contains(current_num)) set.remove(current_num);
-            else if (current_num >= max) {
-                set = add_plus(set, max, current_num);
-                max = current_num;
-            }
-            else if (current_num <= min) {
-                set = add_minus(set, min, current_num);
-                min = current_num;
-            }
+        while (idx != nums.length) {
+            int current = nums[idx];
+            if (current != last + 1) ans = add(ans, last, current);
+            last = current;
+            idx++;
         }
 
-        if (nums.length > max) add_plus(set, max, nums.length+1);
-        if (1 < min) add_minus(set, min, 0);
+        ans = add(ans, last, nums.length + 1);
 
-        return convert(set);
+        return ans;
     }
 }
