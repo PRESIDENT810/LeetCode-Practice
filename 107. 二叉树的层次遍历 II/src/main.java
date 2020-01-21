4a1ab5622a7dd4aff52e9223f393ed606310944d
Jacket <117010420@link.cuhk.edu.cn>
2020-01-21 15:53:36 +0800

    Finish Q107

---
+++ 107. 二叉树的层次遍历 II/src/main.java
@@ -0,0 +1,67 @@
+import com.sun.source.tree.Tree;
+
+import java.util.*;
+
+public class main {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> reverse(Stack<LinkedList<Integer>> ans) {
+        List<List<Integer>> reverse = new LinkedList<>();
+        while (!ans.isEmpty()){
+            reverse.add(ans.pop());
+        }
+
+        return reverse;
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) return new LinkedList<>();
+
+        Queue<TreeNode> tree_queue = new LinkedList<>();
+        Queue<Integer> level_queue = new LinkedList<>();
+
+        tree_queue.add(root);
+        level_queue.add(0);
+        int current_level = 0;
+        LinkedList<Integer> current_list = new LinkedList<>();
+        Stack<LinkedList<Integer>> ans = new Stack<>();
+
+        while (!tree_queue.isEmpty()) {
+            TreeNode current_node = tree_queue.remove();
+            int level = level_queue.remove();
+
+            // add children to the tree_queue & add children level to the level_queue
+            if (current_node.left != null) {
+                tree_queue.add(current_node.left);
+                level_queue.add(level + 1);
+            }
+            if (current_node.right != null) {
+                tree_queue.add(current_node.right);
+                level_queue.add(level + 1);
+            }
+
+            // if go to a new level, use a new list
+            if (level != current_level) {
+                ans.push(current_list);
+                current_list = new LinkedList<>();
+                current_list.add(current_node.val);
+                current_level = level;
+            } else {
+                current_list.add(current_node.val);
+            }
+        }
+        ans.push(current_list);
+        return reverse(ans);
+    }
+}
