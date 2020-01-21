8ca49c945c6d24bffb2cf171101cfc2ba779215e
Jacket <117010420@link.cuhk.edu.cn>
2020-01-21 23:08:50 +0800

    Finish Q112

---
+++ 112. 路径总和/src/main.java
@@ -0,0 +1,31 @@
+public class main {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+
+        return recursion(root, sum);
+    }
+
+    public boolean recursion(TreeNode root, int sum){
+        if (root.left == null && root.right == null) return sum==root.val;
+
+        int current = root.val;
+
+        if (root.left == null) return hasPathSum(root.right, sum-current);
+        if (root.right == null) return hasPathSum(root.left, sum-current);
+
+        return hasPathSum(root.left, sum-current) || hasPathSum(root.right, sum-current);
+    }
+}
