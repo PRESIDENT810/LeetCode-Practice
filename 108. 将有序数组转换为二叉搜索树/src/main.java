b99e7c5007fd24c84964ec52b4c9ceafd8c54c52
Jacket <117010420@link.cuhk.edu.cn>
2020-01-21 21:15:07 +0800

    Finish Q108

--- 108. 将有序数组转换为二叉搜索树/src/main.java
+++ 108. 将有序数组转换为二叉搜索树/src/main.java
@@ -1,6 +1,3 @@
-public class main {
-}
-
 class TreeNode {
     int val;
     TreeNode left;
@@ -11,11 +8,28 @@ class TreeNode {
     }
 }
 
-class Solution {
-    public TreeNode sortedArrayToBST(int[] nums) {
+public class main {
+    public static void main(String args[]){
+        int[] arr = {-10,-3,0,5,9};
+        TreeNode root = new Solution108().sortedArrayToBST(arr);
+    }
+}
 
+class Solution108 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return recursion(nums, 0, nums.length);
     }
 
-    public TreeNode recursion(int[] nums, )
+    public TreeNode recursion(int[] nums, int left_idx, int right_idx){
+        if (left_idx == nums.length || left_idx == right_idx) return null;
+        if (left_idx+1 == right_idx) return new TreeNode(nums[left_idx]);
 
+        int mid_idx = (left_idx+right_idx)/2;
+
+        TreeNode root = new TreeNode(nums[mid_idx]);
+        root.left = recursion(nums, left_idx, mid_idx);
+        root.right = recursion(nums, mid_idx+1, right_idx);
+
+        return root;
+    }
 }
