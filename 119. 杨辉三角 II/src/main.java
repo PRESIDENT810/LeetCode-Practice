851daf6cf23d15de6a0df51daf0691170177413b
Jacket <117010420@link.cuhk.edu.cn>
2020-01-21 23:34:57 +0800

    Finish Q119

--- 119. 杨辉三角 II/src/main.java
+++ 119. 杨辉三角 II/src/main.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class main {
 }
 
-class Solution118 {
+class Solution119 {
     public LinkedList<Integer> next(LinkedList<Integer> last_row) {
         LinkedList<Integer> current_row = new LinkedList<>();
 
@@ -16,18 +16,15 @@ class Solution118 {
         return current_row;
     }
 
-    public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans = new LinkedList<>();
-        if (numRows == 0) return ans;
+    public List<Integer> getRow(int rowIndex) {
         LinkedList<Integer> last_row = new LinkedList<>();
         last_row.add(1);
+        if (rowIndex == 0) return last_row;
 
-        for (int i = 0; i < numRows; i++) {
-            ans.add(last_row);
+        for (int i = 0; i < rowIndex; i++) {
             last_row = next(last_row);
         }
 
-        ans.add(last_row);
-        return ans;
+        return last_row;
     }
 }
