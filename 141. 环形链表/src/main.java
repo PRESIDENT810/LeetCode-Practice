16fc5608380888213fcd40fc071af2a8dcb20836
Jacket <117010420@link.cuhk.edu.cn>
2020-01-23 02:12:07 +0800

    Finish Q141

---
+++ 141. 环形链表/src/main.java
@@ -0,0 +1,30 @@
+public class main {
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+        next = null;
+    }
+}
+
+class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        ListNode current = head;
+        ListNode temp;
+
+        while (current.next != null) {
+            if (current.next == head) return true;
+
+            temp = current.next;
+            current.next = head;
+            current = temp;
+        }
+        return false;
+    }
+}
