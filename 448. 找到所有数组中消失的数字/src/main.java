e10f062440df2ffe6fbe8d0a8587c4387c8a652e
Jacket <117010420@link.cuhk.edu.cn>
2020-01-24 20:59:59 +0800

    Update Q448

--- 448. 找到所有数组中消失的数字/src/main.java
+++ 448. 找到所有数组中消失的数字/src/main.java
@@ -8,30 +8,15 @@ public class main {
 }
 
 class Solution448 {
-    public List<Integer> add(List<Integer> ans, int last, int current) {
-        for (int i = 1; i < current - last; i++) ans.add(i + last);
-
-        return ans;
-    }
-
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        if (nums.length == 0) return new LinkedList<>();
-
-        Arrays.sort(nums);
-        List<Integer> ans = new LinkedList<>();
-
-        int idx = 0;
-        int last = 0;
-
-        while (idx != nums.length) {
-            int current = nums[idx];
-            if (current != last + 1) ans = add(ans, last, current);
-            last = current;
-            idx++;
+        for (int i=0; i<nums.length; i++){
+            int num = nums[i];
+            if (num < 0) num *= -1;
+            if (nums[num-1] > 0) nums[num-1] *= -1;
         }
 
-        ans = add(ans, last, nums.length + 1);
-
+        List<Integer> ans = new LinkedList<>();
+        for (int i=0; i<nums.length; i++) if (nums[i] > 0) ans.add(i+1);
         return ans;
     }
 }
