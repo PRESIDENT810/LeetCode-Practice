cd5d0454c815e9b6729f4ecff45bcb59154c3d70
Jacket <117010420@link.cuhk.edu.cn>
2020-01-30 17:16:00 +0800

    Finish Q169 (hash)

--- 169. 多数元素/src/main.java
+++ 169. 多数元素/src/main.java
@@ -1,11 +1,24 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class main {
 }
 
 class Solution169 {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        Map map = new HashMap();
+
+        for (int i:  nums){
+            if (map.containsKey(i)) map.put(i, (int) map.get(i)+1);
+            else map.put(i, 1);
+        }
+
+        int max = 0;
+        int max_key = 0;
+        int value;
+        for (Object key: map.keySet()){
+            value = (int) map.get(key);
+            if (max < value ) {max = value; max_key = (int) key;}
+        }
+        return max_key;
     }
 }
