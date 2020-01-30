85c35196451857b27baa8644e8eb670a667ae803
Jacket <117010420@link.cuhk.edu.cn>
2020-01-30 23:37:33 +0800

    Fuck ring replacement Fuck this shit Stupid bitch ass Fuck you little fucker

--- 189. 旋转数组/src/main.java
+++ 189. 旋转数组/src/main.java
@@ -1,7 +1,14 @@
+import java.util.Arrays;
+
 public class main {
+    public static void main(String args[]) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        new Solution189().rotate(arr, 4);
+        System.out.print(Arrays.toString(arr));
+    }
 }
 
-class Solution {
+class Solution189 {
     public void reverse(int[] nums, int start, int end){
         int temp;
         while (start != end && start != end+1) {
@@ -14,6 +21,7 @@ class Solution {
     }
 
     public void rotate(int[] nums, int k) {
+        k %= nums.length;
         reverse(nums, 0, nums.length-1);
         reverse(nums, 0, k-1);
         reverse(nums, k, nums.length-1);
