8bde04f562232c1974041ba7e87f7a2eaef262ef
Jacket <117010420@link.cuhk.edu.cn>
2020-01-31 15:53:33 +0800

    Finish Q204 (improved filter)

--- 204. 计数质数/src/main.java
+++ 204. 计数质数/src/main.java
@@ -2,41 +2,22 @@ import java.util.*;
 
 public class main {
     public static void main(String args[]){
-        System.out.print(new Solution204().countPrimes(10000));
+        System.out.print(new Solution204().countPrimes(10));
     }
 }
 
 class Solution204 {
-    public boolean isPrime(int i, boolean[] Primes) {
-        int mark = (int) Math.sqrt(i);
-        for (int num=2; num<mark+1; num++){
-            if (!Primes[num]) continue;
-            if (i%num == 0) return false;
-        }
-        return true;
-    }
-
-    public boolean[] fill(int i, boolean[] Primes){
-        int mark = (int) Math.sqrt(Primes.length)+1;
-        int hole = i*2;
-        while (hole < mark) {
-            Primes[hole] = false;
-            hole += i;
-        }
-        return Primes;
-    }
-
     public int countPrimes(int n) {
         if (n < 2) return 0;
         boolean[] Primes = new boolean[n];
-        for (int i=0; i<n; i++) Primes[i] = true;
-        Primes[0] = false;
-        Primes[1] = false;
         int ans = 0;
 
+
         for (int i=2; i<n; i++){
-            if (!Primes[i]) continue;
-            if (isPrime(i, Primes)) {Primes[i] = true; ans++; Primes = fill(i, Primes);}
+            if (!Primes[i]) {
+                ans++;
+                for (int j=2; i*j<n; j++) Primes[i*j] = true;
+            }
         }
         return ans;
     }
