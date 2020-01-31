21e16332ca567710aeca59d5a6e0c21817f1e422
Jacket <117010420@link.cuhk.edu.cn>
2020-01-31 16:46:53 +0800

    Finish Q205 (one Hashmaps)

--- 205. 同构字符串/src/main.java
+++ 205. 同构字符串/src/main.java
@@ -6,7 +6,6 @@ public class main {
 class Solution {
     public boolean isIsomorphic(String s, String t) {
         Map map1 = new HashMap();
-        Map map2 = new HashMap();
 
         if (s.length() != t.length()) return false;
 
@@ -19,11 +18,9 @@ class Solution {
 
             if (map1.containsKey(c1)) {
                 if ((char) map1.get(c1) != c2) return false;
-            } else if (map2.containsKey(c2)) {
-                if ((char) map2.get(c2) != c1) return false;
-            } else {
+            } else if (map1.containsValue(c2)) return false;
+            else {
                 map1.put(c1, c2);
-                map2.put(c2, c1);
             }
         }
         return true;
