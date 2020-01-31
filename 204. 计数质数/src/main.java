2a6ea1b262b896fac4bcd674b80400fa5e3026f0
Jacket <117010420@link.cuhk.edu.cn>
2020-01-31 15:45:18 +0800

    Finish Q204 (filter)

--- 204. 计数质数/src/main.java
+++ 204. 计数质数/src/main.java
@@ -1,25 +1,42 @@
+import java.util.*;
 
 public class main {
+    public static void main(String args[]){
+        System.out.print(new Solution204().countPrimes(10000));
+    }
 }
 
 class Solution204 {
-    public boolean isPrime(int n){
-        if (n==3 || n==5 || n==7) return true;
-        if (n==4 || n==6) return false;
-        if (n%2 == 0 || n%3 == 0 || n%5 == 0) return false;
-
-        for (int i=7; i<Math.sqrt(n)+1; i+=2){
-            if (i%3 == 0 || i%5 == 0) continue;
-            if (n%i == 0) return false;
+    public boolean isPrime(int i, boolean[] Primes) {
+        int mark = (int) Math.sqrt(i);
+        for (int num=2; num<mark+1; num++){
+            if (!Primes[num]) continue;
+            if (i%num == 0) return false;
         }
         return true;
     }
 
+    public boolean[] fill(int i, boolean[] Primes){
+        int mark = (int) Math.sqrt(Primes.length)+1;
+        int hole = i*2;
+        while (hole < mark) {
+            Primes[hole] = false;
+            hole += i;
+        }
+        return Primes;
+    }
+
     public int countPrimes(int n) {
-        if (n<=2) return 0;
-        int ans = 1;
-        for (int i=3; i<n; i+=2){
-            if (isPrime(i)) ans++;
+        if (n < 2) return 0;
+        boolean[] Primes = new boolean[n];
+        for (int i=0; i<n; i++) Primes[i] = true;
+        Primes[0] = false;
+        Primes[1] = false;
+        int ans = 0;
+
+        for (int i=2; i<n; i++){
+            if (!Primes[i]) continue;
+            if (isPrime(i, Primes)) {Primes[i] = true; ans++; Primes = fill(i, Primes);}
         }
         return ans;
     }
