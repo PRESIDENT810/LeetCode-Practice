ac6988beba377068f5242c0b2d65edde0e0d1955
Jacket <117010420@link.cuhk.edu.cn>
2020-02-01 12:52:27 +0800

    Finish Q3 (Array is faster than Hashmap)

--- 3. 无重复字符的最长子串/src/main.java
+++ 3. 无重复字符的最长子串/src/main.java
@@ -3,7 +3,7 @@ import java.util.*;
 class main {
     public static void main(String[] args) {
         Solution3 solu = new Solution3();
-        int result = solu.lengthOfLongestSubstring("abcabcbb");
+        int result = solu.lengthOfLongestSubstring("au");
         System.out.println(result);
     }
 }
@@ -16,14 +16,13 @@ class Solution3 {
         int ptr2 = 0;
 
         int max = 1;
-        Map map = new HashMap();
+        int[] map = new int[128];
+        for (int i=0; i<128; i++) map[i] = -1;
 
         while (ptr2 != arr.length) {
-            if (map.containsKey(arr[ptr2])){
-                if ((int) map.get(arr[ptr2]) >= ptr1) ptr1 = (int) map.get(arr[ptr2])+1;
-            }
-            map.put(arr[ptr2], ptr2);
-            if (ptr2 - ptr1 + 1 > max) max = ptr2-ptr1+1;
+            if (map[arr[ptr2]-' '] >= ptr1) ptr1 = map[arr[ptr2]-' '] + 1;
+            map[arr[ptr2]-' '] = ptr2;
+            if (ptr2 - ptr1 + 1 > max) max = ptr2 - ptr1 + 1;
             ptr2++;
         }
         return max;
