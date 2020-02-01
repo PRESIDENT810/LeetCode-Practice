e71302c4b018e57c7105391737c144a3ec8377f1
Jacket <117010420@link.cuhk.edu.cn>
2020-02-01 12:44:44 +0800

    Finish Q3 (I'm so noob...)

--- 3. 无重复字符的最长子串/src/main.java
+++ 3. 无重复字符的最长子串/src/main.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.math.*;
 
 class main {
     public static void main(String[] args) {
@@ -10,54 +9,23 @@ class main {
 }
 
 class Solution3 {
-    public boolean LongestOnTheEnd(String s, int len) {
-        String end_sub = s.substring(s.length() - len);
-        Set end_set = new HashSet();
-
-        for (int i = 0; i < len; i++) {
-            if (end_set.contains(end_sub.charAt(i))) return false;
-            end_set.add(end_sub.charAt(i));
-        }
-        return true;
-    }
-
-    public Set EndSet(String s, int len) {
-        String end_sub = s.substring(s.length() - len);
-        Set end_set = new HashSet();
-
-        for (int i = 0; i < len; i++) {
-            end_set.add(end_sub.charAt(i));
-        }
-        return end_set;
-    }
-
-    public Set setOfLongestSubstring(String s) {
-        int len = s.length();
-
-        Set sub_set = new HashSet();
-        String temp_s;
-
-        for (int i = 1; i < len + 1; i++) {
-            temp_s = s.substring(0, i);
-
-            if (i == 1) {
-                sub_set.add(s.charAt(0));
-                continue;
-            }
-
-            String sub_s = s.substring(0, i - 1);
-
-            if (LongestOnTheEnd(sub_s, sub_set.size())) {
-                sub_set = EndSet(temp_s, sub_set.size() + 1);
-            }
-        }
-
-        return sub_set;
-    }
-
-
     public int lengthOfLongestSubstring(String s) {
-        if (s.length() == 0) return 0;
-        return setOfLongestSubstring(s).size();
+        char[] arr = s.toCharArray();
+        if (arr.length == 0) return 0;
+        int ptr1 = 0;
+        int ptr2 = 0;
+
+        int max = 1;
+        Map map = new HashMap();
+
+        while (ptr2 != arr.length) {
+            if (map.containsKey(arr[ptr2])){
+                if ((int) map.get(arr[ptr2]) >= ptr1) ptr1 = (int) map.get(arr[ptr2])+1;
+            }
+            map.put(arr[ptr2], ptr2);
+            if (ptr2 - ptr1 + 1 > max) max = ptr2-ptr1+1;
+            ptr2++;
+        }
+        return max;
     }
 }
