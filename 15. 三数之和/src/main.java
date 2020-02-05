04ed384beb771fb819be57f5fc45093dced2d230
Jacket <117010420@link.cuhk.edu.cn>
2020-02-05 22:25:54 +0800

    Finish Q15（我操了）

--- 15. 三数之和/src/main.java
+++ 15. 三数之和/src/main.java
@@ -2,24 +2,42 @@ import java.util.*;
 
 public class main {
     public static void main(String args[]){
-        int[] arr = {-1, 0, 1, 2, -1, -4};
+        int[] arr = {3,0,-2,-1,1,2};
         List<List<Integer>> all_ans = new Solution15().threeSum(arr);
     }
 }
 
 class Solution15 {
-    // TODO: 我他娘真是不会这个题
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<Integer> targets = new HashSet<>();
         List<List<Integer>> all_ans = new LinkedList<>();
+        Set<List<Integer>> ans_set = new HashSet<>();
+        if (nums.length < 3) return all_ans;
 
-        for (int num: nums){
-            targets.add(-1*num);
+        Arrays.sort(nums);
+        int left = 0;
+        int mid = 1;
+        int right = 2;
+        int sum;
+
+        while (mid != nums.length-1){
+            if (left == -1 || right == nums.length) {
+                mid++; left = mid-1; right = mid+1;
+                continue;
+            }
+
+            sum = nums[left]+nums[mid]+nums[right];
+            if (sum == 0) {
+                LinkedList<Integer> ans = new LinkedList<>();
+                ans.add(nums[left]); ans.add(nums[mid]);ans.add(nums[right]);
+                ans_set.add(ans);
+                left--; right++;
+                continue;
+            }
+            else if (sum > 0) left--;
+            else right++;
         }
 
-        for (Integer target: targets){
-
-        }
+        for (List<Integer> ans: ans_set) all_ans.add(ans);
         return all_ans;
     }
 }
