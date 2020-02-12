c86bd97627f1c2d9fc5aac9f700cc241c9941aff
Jacket <117010420@link.cuhk.edu.cn>
2020-02-13 00:25:36 +0800

    Finish Q226

---
+++ 226. 翻转二叉树/src/main.java
@@ -0,0 +1,24 @@
+public class main {
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x) {
+        val = x;
+    }
+}
+
+class Solution226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        TreeNode temp = left;
+        root.left = right;
+        root.right = temp;
+        return root;
+    }
+}
