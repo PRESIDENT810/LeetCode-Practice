ef60bb89053d46b603ff0ab756ba34a32d79204c
Jacket <117010420@link.cuhk.edu.cn>
2020-02-13 00:56:03 +0800

    Finish Q237

---
+++ 237. 删除链表中的节点/src/main.java
@@ -0,0 +1,18 @@
+public class main {
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
+
+class Solution237 {
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
