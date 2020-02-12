34c53be02ae2615ed58370aa0882870229c6d095
Jacket <117010420@link.cuhk.edu.cn>
2020-02-13 00:21:43 +0800

    Finish Q263 (O(lgn))

--- 263. 丑数/src/main.java
+++ 263. 丑数/src/main.java
@@ -5,38 +5,14 @@ public class main {
 }
 
 class Solution263 {
-    public boolean alternative(int num) {
-        for (int factor : new int[]{2, 3, 5}) {
-            while (num % factor == 0) num /= factor;
-        }
-        return num == 1;
-    }
-
     public boolean isUgly(int num) {
         if (num < 0) return false;
         if (num == 0) return false;
         if (num == 1) return true;
-        if (num > Math.pow(2, 24)) return alternative(num);
-
-        int[] arr = new int[num + 1];
-        int base;
 
         for (int factor : new int[]{2, 3, 5}) {
-            base = 0;
-            while (base <= num) {
-                arr[base] = -1;
-                base += factor;
-            }
+            while (num % factor == 0) num /= factor;
         }
-
-        for (int i = 7; i < num; i++) {
-            if (arr[i] != 0) continue;
-            base = i;
-            while (base <= num) {
-                arr[base] = 1;
-                base += i;
-            }
-        }
-        return arr[num] == -1;
+        return num == 1;
     }
 }
