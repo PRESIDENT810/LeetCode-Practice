e10e9373364e1f093c757f704b80f448c48413d3
Jacket <117010420@link.cuhk.edu.cn>
2020-02-14 00:58:10 +0800

    Finish Q326

---
+++ 326. 3的幂/src/main.java
@@ -0,0 +1,10 @@
+public class main {
+}
+
+class Solution326 {
+    public boolean isPowerOfThree(int n) {
+        if (n == 0) return false;
+        while (n%3 == 0) n /= 3;
+        return n == 1;
+    }
+}
