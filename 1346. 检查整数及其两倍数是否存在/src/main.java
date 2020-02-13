f8c3fa55473cfd603d46dc2a2169f7cdc7bc080f
Jacket <117010420@link.cuhk.edu.cn>
2020-02-14 00:17:00 +0800

    Finish Q268

--- 1346. 检查整数及其两倍数是否存在/src/main.java
+++ 1346. 检查整数及其两倍数是否存在/src/main.java
@@ -7,8 +7,8 @@ class Solution1346 {
     public boolean checkIfExist(int[] arr) {
         Set set = new HashSet();
         for (int num : arr) {
-            if (set.contains((float) num*2) || set.contains((float) num/2)) return true;
-            set.add((float) num);
+            if (set.contains(num*2) || set.contains(num/2)) return true;
+            set.add(num);
         }
         return false;
     }
---
+++ 268. 缺失数字/src/main.java
@@ -0,0 +1,12 @@
+public class main {
+}
+
+class Solution268 {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        for (int num : nums){
+            sum += num;
+        }
+        return nums.length*(nums.length+1)/2 - sum;
+    }
+}
