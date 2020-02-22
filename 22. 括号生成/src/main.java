181cb02358f7db8b1f05d73268df13637264b81a
Jacket <117010420@link.cuhk.edu.cn>
2020-02-22 21:10:50 +0800

    Finish Q22 (slightly improved)

--- 22. 括号生成/src/main.java
+++ 22. 括号生成/src/main.java
@@ -7,60 +7,42 @@ public class main {
 }
 
 class Node {
-    Node parent;
     Node left;
     Node right;
     int l_cnt;
     int r_cnt;
     int layer;
-    char parenthesis;
+    StringBuilder str_builder = new StringBuilder();
 }
 
 class Solution22 {
-    List<Node> leaves = new LinkedList<>();
+    List<String> ans = new LinkedList<>();
 
     public List<String> generateParenthesis(int n) {
         Node root = new Node();
         root.layer = 1;
         root.l_cnt = 1;
+        root.str_builder = new StringBuilder("(");
         root = recursion(root, 2 * n);
-        List<String> ans = backtrack(root);
-        return ans;
-    }
-
-    public List<String> backtrack(Node node) {
-        List<String> ans = new LinkedList<>();
-        StringBuilder str;
-        for (Node leave : this.leaves){
-            str = new StringBuilder();
-            while (leave.parent != null){
-                str.append(leave.parenthesis);
-                leave = leave.parent;
-            }
-            str.append(')');
-            ans.add(new String(str));
-        }
-        return ans;
+        return this.ans;
     }
 
     public Node recursion(Node parent, int depth) {
         if (parent.layer == depth) {
-            this.leaves.add(parent);
+            this.ans.add(new String(parent.str_builder));
             return parent;
         }
 
         Node l_child = new Node();
-        l_child.parent = parent;
         l_child.l_cnt = parent.l_cnt + 1;
         l_child.r_cnt = parent.r_cnt;
         l_child.layer = parent.layer + 1;
-        l_child.parenthesis = ')';
+        l_child.str_builder = new StringBuilder(parent.str_builder).append('(');
         Node r_child = new Node();
-        r_child.parent = parent;
         r_child.l_cnt = parent.l_cnt;
         r_child.r_cnt = parent.r_cnt + 1;
         r_child.layer = parent.layer + 1;
-        r_child.parenthesis = '(';
+        r_child.str_builder = new StringBuilder(parent.str_builder).append(')');
 
         if (parent.l_cnt == depth / 2) {
             parent.right = recursion(r_child, depth);
