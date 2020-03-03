944bfdfe6730146c799d15ef6547d0c7501e9be1
Jacket <117010420@link.cuhk.edu.cn>
2020-03-03 19:40:11 +0800

    Update Q88

--- 88. 合并两个有序数组/src/main.java
+++ 88. 合并两个有序数组/src/main.java
@@ -1,39 +1,38 @@
 public class main {
-    public static void main(String args[]){
-        int[] arr1 = {1,2,3,0,0,0};
-        int[] arr2 = {2,5,6};
+    public static void main(String args[]) {
+        int[] arr1 = {0};
+        int[] arr2 = {1};
         Solution88 solu = new Solution88();
-        solu.merge(arr1, 3, arr2, 3);
+        solu.merge(arr1, 0, arr2, 1);
     }
 }
 
 class Solution88 {
-    public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int ptr1 = m - 1;
-        int ptr2 = n - 1;
-
-        for (int i = 0; i < nums1.length; i++) {
-            int idx = nums1.length - i - 1;
-
-            if (ptr1 == -1) {
-                nums1[idx] = nums2[ptr2];
-                ptr2--;
+    public void merge(int[] A, int m, int[] B, int n) {
+        int end = A.length - 1;
+        while (end != -1) {
+            if (m == 0) {
+                A[end] = B[n - 1];
+                n--;
+                end--;
                 continue;
             }
-            else if (ptr2 == -1) {
-                nums1[idx] = nums1[ptr1];
-                ptr1--;
+            if (n == 0) {
+                A[end] = A[m - 1];
+                m--;
+                end--;
                 continue;
             }
 
-            if (nums1[ptr1] > nums2[ptr2]) {
-                nums1[idx] = nums1[ptr1];
-                ptr1--;
+            if (A[m - 1] > B[n - 1]) {
+                A[end] = A[m - 1];
+                m--;
+                end--;
             } else {
-                nums1[idx] = nums2[ptr2];
-                ptr2--;
+                A[end] = B[n - 1];
+                n--;
+                end--;
             }
-
         }
     }
 }
