cfd4b5934c7f77e48bf39fc34bafc7a6a80bca26
Jacket <117010420@link.cuhk.edu.cn>
2020-03-06 13:17:59 +0800

    Update Q_57 (better...)

--- 面试题57 - II. 和为s的连续正数序列/src/main.java
+++ 面试题57 - II. 和为s的连续正数序列/src/main.java
@@ -8,20 +8,20 @@ public class main {
 
 class Solution_57 {
     public int[][] findContinuousSequence(int target) {
-        int ptr1 = 1;
-        int ptr2 = 1;
         List<int[]> list = new LinkedList<>();
-        while (ptr1 <= target / 2 + 1) {
-            int sum = (ptr1 + ptr2) * (ptr1 - ptr2 + 1) / 2;
-            if (sum < target) ptr1++;
-            else if (sum > target) ptr2++;
-            else {
-                int[] seq = new int[ptr1-ptr2+1];
-                for (int i=0; i<ptr1-ptr2+1; i++) seq[i] = ptr2+i;
+        int width = 1;
+        while (target > 0){
+            target -= width-1;
+            if (target%width == 0 && width != 1){
+                int head = target/width;
+                if (head == 0) continue;
+                int[] seq = new int[width];
+                for (int i=0; i<width; i++) seq[i] = i+head;
                 list.add(seq);
-                ptr1++;
             }
+            width++;
         }
+        Collections.reverse(list);
         int[][] ans = new int[list.size()][];
         return list.toArray(ans);
     }
