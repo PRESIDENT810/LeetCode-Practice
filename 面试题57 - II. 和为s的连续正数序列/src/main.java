1640923a9a50530012a529a2d2adf2b5f0c3b240
Jacket <117010420@link.cuhk.edu.cn>
2020-03-06 13:03:52 +0800

    Finish Q_57 (double ptr)

--- 面试题57 - II. 和为s的连续正数序列/src/main.java
+++ 面试题57 - II. 和为s的连续正数序列/src/main.java
@@ -2,29 +2,27 @@ import java.util.*;
 
 public class main {
     public static void main(String args[]) {
-        new Solution_57().findContinuousSequence(10);
+        new Solution_57().findContinuousSequence(9);
     }
 }
 
 class Solution_57 {
     public int[][] findContinuousSequence(int target) {
-        Map<Integer, Integer> map = new HashMap<>();
+        int ptr1 = 1;
+        int ptr2 = 1;
         List<int[]> list = new LinkedList<>();
-        for (int tail = 0; tail < target / 2 + 2; tail++) {
-            int sum = (1 + tail) * tail / 2;
-            if (map.containsKey(sum - target)) {
-                int head = map.get(sum-target);
-                list.add(generate(head, tail));
+        while (ptr1 <= target / 2 + 1) {
+            int sum = (ptr1 + ptr2) * (ptr1 - ptr2 + 1) / 2;
+            if (sum < target) ptr1++;
+            else if (sum > target) ptr2++;
+            else {
+                int[] seq = new int[ptr1-ptr2+1];
+                for (int i=0; i<ptr1-ptr2+1; i++) seq[i] = ptr2+i;
+                list.add(seq);
+                ptr1++;
             }
-            map.put(sum, tail);
         }
         int[][] ans = new int[list.size()][];
         return list.toArray(ans);
-    }
-
-    public int[] generate(int head, int tail) {
-        int[] seq = new int[tail-head];
-        for (int i=0; i<tail-head; i++) seq[i] = head+i+1;
-        return seq;
     }
 }
