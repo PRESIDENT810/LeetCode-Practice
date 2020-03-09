f43fa17b50dcd5077c2d47653522224bdd032796
Jacket <117010420@link.cuhk.edu.cn>
2020-03-09 13:34:37 +0800

    Finish Q322 (打扰了。。。是我犯蠢了)

--- 322. 零钱兑换/src/main.java
+++ 322. 零钱兑换/src/main.java
@@ -2,14 +2,13 @@ import java.util.*;
 
 public class main {
     public static void main(String args[]) {
-        int[] arr = {357,239,73,52};
-        System.out.print(new Solution322().coinChange(arr, 9832));
+        int[] arr = {1,2222};
+        System.out.print(new Solution322().coinChange(arr, 2));
     }
 }
 
 class Solution322 {
     int[] map;
-    Set<Integer> set = new HashSet<>();
 
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) return 0;
@@ -19,27 +18,20 @@ class Solution322 {
         for (int coin : coins) {
             if (coin > amount) continue;
             this.map[coin] = 1;
-            this.set.add(coin);
             if (coin < min) min = coin;
         }
-        boolean add = false;
-
-
 
         for (int i = min; i < this.map.length; i++) {
             int sub_amount = i;
-            add = false;
-            for (int available : this.set) {
-                if (this.map[available] != 1) continue;
-                if (available*2 <= sub_amount+1 && this.set.contains(sub_amount - available)) {
-                    add = true;
-                    int cnt = this.map[available] + this.map[sub_amount - available];
+            for (int coin : coins) {
+                if (coin >= amount) continue;
+                if (coin*2 <= sub_amount+1 && this.map[sub_amount - coin] != 0) {
+                    int cnt = this.map[coin] + this.map[sub_amount - coin];
                     if (this.map[i] == 0 || this.map[i] > cnt) {
                         this.map[i] = cnt;
                     }
                 }
             }
-            if (add) this.set.add(sub_amount);
         }
         return this.map[amount] == 0 ? -1 : this.map[amount];
     }
