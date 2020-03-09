3537a1bb2f684f8724f553d17369b15e34139c21
Jacket <117010420@link.cuhk.edu.cn>
2020-03-09 13:45:55 +0800

    Finish Q322 (slightly improved)

--- 322. 零钱兑换/src/main.java
+++ 322. 零钱兑换/src/main.java
@@ -8,31 +8,29 @@ public class main {
 }
 
 class Solution322 {
-    int[] map;
-
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) return 0;
-        this.map = new int[amount+1];
+
+        int[] map;
+        map = new int[amount+1];
+        Arrays.fill(map, amount+1);
 
         int min = coins[0];
         for (int coin : coins) {
             if (coin > amount) continue;
-            this.map[coin] = 1;
+            map[coin] = 1;
             if (coin < min) min = coin;
         }
 
-        for (int i = min; i < this.map.length; i++) {
+        for (int i = min; i < map.length; i++) {
             int sub_amount = i;
             for (int coin : coins) {
                 if (coin >= amount) continue;
-                if (coin*2 <= sub_amount+1 && this.map[sub_amount - coin] != 0) {
-                    int cnt = this.map[coin] + this.map[sub_amount - coin];
-                    if (this.map[i] == 0 || this.map[i] > cnt) {
-                        this.map[i] = cnt;
-                    }
+                if (coin*2 <= sub_amount+1) {
+                    map[i] = Math.min(map[coin] + map[sub_amount - coin], map[i]);
                 }
             }
         }
-        return this.map[amount] == 0 ? -1 : this.map[amount];
+        return map[amount] == amount+1 ? -1 : map[amount];
     }
 }
