364696f79f08d5221f918c2b5594a2af306881ea
Jacket <117010420@link.cuhk.edu.cn>
2020-03-09 12:57:00 +0800

    Not finish Q322 (still exceed time limit...)

--- 322. 零钱兑换/src/main.java
+++ 322. 零钱兑换/src/main.java
@@ -2,8 +2,8 @@ import java.util.*;
 
 public class main {
     public static void main(String args[]) {
-        int[] arr = {1, 2, 5};
-        System.out.print(new Solution322().coinChange(arr, 11));
+        int[] arr = {357,239,73,52};
+        System.out.print(new Solution322().coinChange(arr, 9832));
     }
 }
 
@@ -14,17 +14,23 @@ class Solution322 {
     public int coinChange(int[] coins, int amount) {
         if (amount == 0) return 0;
         this.map = new int[amount+1];
+
+        int min = coins[0];
         for (int coin : coins) {
             if (coin > amount) continue;
             this.map[coin] = 1;
             this.set.add(coin);
+            if (coin < min) min = coin;
         }
         boolean add = false;
-        for (int i = 1; i < this.map.length; i++) {
+
+
+
+        for (int i = min; i < this.map.length; i++) {
             int sub_amount = i;
             add = false;
             for (int available : this.set) {
-                if (this.set.contains(sub_amount - available)) {
+                if (available*2 <= sub_amount+1 && this.set.contains(sub_amount - available)) {
                     add = true;
                     int cnt = this.map[available] + this.map[sub_amount - available];
                     if (this.map[i] == 0 || this.map[i] > cnt) {
