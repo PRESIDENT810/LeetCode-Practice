ce2bce963c052311da73c0abff7dceed413778e3
Jacket <117010420@link.cuhk.edu.cn>
2020-03-12 23:35:33 +0800

    Finish Q1071 (quite easy actually...)

--- 1071. 字符串的最大公因子/src/main.java
+++ 1071. 字符串的最大公因子/src/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String args[]){
-        String str1 =  "ABCABC";
-        String str2 = "ABC";
+        String str1 =  "TAUXXTAUXXTAUXXTAUXXTAUXX";
+        String str2 = "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX";
         System.out.print(new Solution1071().gcdOfStrings(str1, str2));
     }
 }
@@ -10,29 +10,39 @@ class Solution1071 {
     public String gcdOfStrings(String str1, String str2) {
         char[] s1 = str1.toCharArray();
         char[] s2 = str2.toCharArray();
-
-        int start1 = s1.length-1;
-        int start2 = s2.length-1;
-        int ptr1 = start1;
-        int ptr2 = start2;
-
-        while (ptr1 != 0 || ptr2 != 0) {
-            if (s1[ptr1] != s2[ptr2]) return "";
-            if (ptr2 == 0) {
-                ptr2 = start2;
-                ptr1--;
-                start1 = ptr1;
-                continue;
-            }
-            if (ptr1 == 0) {
-                ptr1 = start1;
-                ptr2--;
-                start2 = ptr2;
-                continue;
-            }
-            ptr1--;
-            ptr2--;
+        char[] temp;
+        if (str1.length() < str2.length()){
+            temp = s1;
+            s1 = s2;
+            s2 = temp;
         }
-        return str1.substring(0, start1+1);
+
+        int ptr1 = 0;
+        int ptr2 = 0;
+
+        while (ptr1 != s1.length){
+            if (s1[ptr1] != s2[ptr2]) return "";
+            ptr1++;
+            ptr2++;
+            if (ptr2 == s2.length) ptr2 = 0;
+        }
+
+        return str1.substring(0, GCD(s2.length, ptr2));
+    }
+
+    public int GCD(int n1, int n2){
+        if (n2 == 0) return n1;
+        while (true){
+            if (n1 > n2){
+                if (n1%n2 == 0) return n2;
+                n1 %= n2;
+                continue;
+            }
+            else{
+                if (n2%n1 == 0) return n1;
+                n2 %= n1;
+                continue;
+            }
+        }
     }
 }
