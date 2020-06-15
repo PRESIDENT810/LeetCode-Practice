af6f87dc780159d3b3cb8d16aa1baf82f026a732
Jacket <117010420@link.cuhk.edu.cn>
2020-06-15 23:43:28 +0800

    update

--- 面试题50. 第一个只出现一次的字符/src/main.java
+++ 面试题50. 第一个只出现一次的字符/src/main.java
@@ -1,33 +1,29 @@
 class main {
-    public static void main(String args[]){
-        String s = "cc";
+    public static void main(String args[]) {
+        String s = "leetcode";
         System.out.print(new Solution_50().firstUniqChar(s));
     }
 }
 
 class Solution_50 {
     public char firstUniqChar(String s) {
-        int[] occur = new int[26];
-        int[] order = new int[26];
-        for (int i=0; i<26; i++){
-            occur[i] = -1;
-        }
+        if (s.length() == 0) return ' ';
+
+        int[] map = new int[27];
         char[] arr = s.toCharArray();
 
-        for (int i=0; i<arr.length; i++){
-            // -1: not occur; 0: occur once; 1: occur more than once
-            if (occur[(int) arr[i] - 97] == -1) {
-                occur[(int) arr[i] - 97] = 0;
-                order[(int) arr[i] - 97] = i;
-            }
-            else if (occur[(int) arr[i] - 97] == 0) {
-                occur[(int) arr[i] - 97] = 1;
+        for (int i = 0; i < arr.length; i++) {
+            // 0: not occur; positive: occur once; negative: occur more than once
+            if (map[(int) arr[i] - 97] == 0) {
+                map[(int) arr[i] - 97] = i+1;
+            } else if (map[(int) arr[i] - 97] > 0) {
+                map[(int) arr[i] - 97] *= -1;
             }
         }
-        int min = arr.length+1;
-        for (int i=0; i<26; i++){
-            if (occur[i] == 0) min = min < order[i] ? min : order[i];
+        int min = arr.length + 1;
+        for (int i = 0; i < 26; i++) {
+            if (map[i] > 0) min = min < map[i] ? min : map[i];
         }
-        return min != arr.length+1 ? arr[min] : ' ';
+        return min != arr.length + 1 ? arr[min-1] : ' ';
     }
 }
