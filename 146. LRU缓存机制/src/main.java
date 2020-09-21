0b18f73ce74a51e936ac518594692fd68b4cd9d0
Jacket <117010420@link.cuhk.edu.cn>
2020-09-21 20:39:33 +0800

    Update Q146

--- 146. LRU缓存机制/src/main.java
+++ 146. LRU缓存机制/src/main.java
@@ -12,47 +12,72 @@ class main {
     }
 }
 
-class LRUCache {
-    int capacity;
-    List<Integer> stack = new LinkedList<>();
-    Map<Integer, Integer> map = new HashMap<>();
-
-    public LRUCache(int capacity) {
-        this.capacity = capacity;
-    }
-
-    public int get(int key) {
-        int index = 0;
-        for (int i : this.stack) {
-            if (i == key) {
-                this.stack.remove(index);
-                this.stack.add(i);
-                return this.map.get(i);
-            }
-            index++;
-        }
-        return -1;
-    }
-
-    public void put(int key, int value) {
-        if (this.map.containsKey(key)) { // key in cache
-            this.map.put(key, value);
-            this.stack.remove((Integer) key);
-            this.stack.add(key);
-        } else { // key not in cache
-            if (this.stack.size() == this.capacity) {
-                int removed_key = this.stack.remove(0);
-                this.map.remove(removed_key);
-            }
-            this.stack.add(key);
-            this.map.put(key, value);
-        }
+class Node{
+    Node prev, next;
+    int key;
+    int value;
+    Node(){}
+    Node(int key, int value){
+        this.key = key;
+        this.value = value;
     }
 }
 
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
+class LRUCache {
+    Map<Integer, Node> map = new HashMap<>();
+    Node Head, Tail;
+    int capacity;
+    int size;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.Head = new Node();
+        this.Tail = new Node();
+        this.Head.next = this.Tail;
+        this.Tail.prev = this.Head;
+    }
+
+    public int get(int key) {
+        // if key already in cache
+        if (this.map.get(key) != null){
+            int value =  this.map.get(key).value;
+            remove(key);
+            add(key, value);
+            return value;
+        }
+        else return -1;
+    }
+
+    public void put(int key, int value) {
+        // if key already in cache
+        if (this.map.containsKey(key)){
+            remove(key);
+            add(key, value);
+        }
+        // if key not in cache
+        else{
+            if (this.size == this.capacity){ // cache full
+                remove(this.Head.next.key);
+            }
+            add(key, value);
+        }
+    }
+
+    public void remove(int key){
+        this.size--;
+        Node removed_node = this.map.get(key);
+        removed_node.prev.next = removed_node.next;
+        removed_node.next.prev = removed_node.prev;
+        this.map.remove(key);
+    }
+
+    public void add(int key, int value){
+        this.size++;
+        Node added_node = new Node(key, value);
+        this.Tail.prev.next = added_node;
+        added_node.prev = this.Tail.prev;
+        added_node.next = this.Tail;
+        this.Tail.prev = added_node;
+        this.map.put(key, added_node);
+    }
+}
