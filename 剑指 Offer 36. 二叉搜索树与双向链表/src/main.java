51eda37251f32160fbbba754f0f3d12f7a7131b1
Jacket <117010420@link.cuhk.edu.cn>
2020-10-21 12:06:25 +0800

    Update Q剑指36

--- 剑指 Offer 36. 二叉搜索树与双向链表/src/main.java
+++ 剑指 Offer 36. 二叉搜索树与双向链表/src/main.java
@@ -1,5 +1,5 @@
 class main {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Node root = new Node(4);
         Node left = new Node(2);
         Node right = new Node(5);
@@ -17,63 +17,54 @@ class Node {
     public Node left;
     public Node right;
 
-    public Node() {}
+    public Node() {
+    }
 
     public Node(int _val) {
         val = _val;
     }
 
-    public Node(int _val,Node _left,Node _right) {
+    public Node(int _val, Node _left, Node _right) {
         val = _val;
         left = _left;
         right = _right;
     }
 };
 
-class EndPoints{
-    EndPoints(Node L_endPoint, Node R_endPoint){
+class EndPoints {
+    EndPoints(Node L_endPoint, Node R_endPoint) {
         this.L_endPoint = L_endPoint;
         this.R_endPoint = R_endPoint;
     }
+
     public Node L_endPoint;
     public Node R_endPoint;
 }
 
 class Solution剑指36 {
+    Node dummy, crt;
+
     public Node treeToDoublyList(Node root) {
         if (root == null) return root;
-        EndPoints endPoints = recursion(root);
-        Node L = endPoints.L_endPoint;
-        Node R = endPoints.R_endPoint;
-        L.left = R;
-        R.right = L;
-        return L;
+        this.dummy = new Node(-1);
+        this.crt = this.dummy;
+        dfs(root);
+        // while (dummy != null){
+        //     System.out.print(dummy.val);
+        //     dummy = dummy.right;
+        // }
+        crt.right = dummy.right;
+        dummy.right.left = crt;
+        return this.dummy.right;
     }
 
-    public EndPoints recursion(Node root){
-        // handle left child
-        Node LL, LR, RL, RR;
-        if (root.left == null){
-            LL = root;
-        }
-        else {
-            EndPoints L_EndPoints = recursion(root.left);
-            LL = L_EndPoints.L_endPoint;
-            LR = L_EndPoints.R_endPoint;
-            LR.right = root;
-            root.left = LR;
-        }
-        // handle right child
-        if (root.right == null){
-            RR = root;
-        }
-        else {
-            EndPoints R_EndPoints = recursion(root.right);
-            RL = R_EndPoints.L_endPoint;
-            RR = R_EndPoints.R_endPoint;
-            RL.left = root;
-            root.right = RL;
-        }
-        return new EndPoints(LL, RR);
+    public void dfs(Node root) {
+        if (root == null) return;
+        // make left child tree a double list and append on crt
+        dfs(root.left);
+        this.crt.right = root;
+        root.left = this.crt;
+        this.crt = root;
+        dfs(root.right);
     }
 }
