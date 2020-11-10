22648ac6beaa5174dec6e67acc42dc22980a5803
Jacket <117010420@link.cuhk.edu.cn>
2020-11-10 15:40:43 +0800

    Update Q80

--- 80. 删除排序数组中的重复项 II/src/main.java
+++ 80. 删除排序数组中的重复项 II/src/main.java
@@ -12,33 +12,20 @@ class Solution80 {
         if (nums.length == 0) return 0;
         Map<Integer, Integer> map = new HashMap<>();
         int cnt = 0;
+        int offset = 0;
         int last = nums[0];
         for (int i=0; i<nums.length; i++){
             if (nums[i] == last){ // same number
                 cnt++;
             }
             else{ // different number
-                map.put(last, cnt);
+                if (cnt > 2) offset += cnt-2; // update offset
                 last = nums[i];
                 cnt = 1;
             }
-        }
-        map.put(last, cnt);
-        // for (int key: map.keySet())System.out.printf("Key: %d, value: %d\n", key, map.get(key));
-        int offset = 0;
-        last = nums[0];
-        int value = map.get(last);
-        for (int i=0; i<nums.length; i++){
-            if (nums[i] != last){ // different number
-                value = map.get(last);
-                if (value > 2) offset += value-2; // update offset
-                last = nums[i]; // update last to current number
-            }
             move(nums, i, offset); // move nums[i] back offset
         }
-        // do it again in case the last section is duplicated
-        value = map.get(last);
-        if (value > 2) offset += value-2; // update offset
+        if (cnt > 2) offset += cnt-2; // update offset
         return nums.length-offset;
     }
 
